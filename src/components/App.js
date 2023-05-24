36ad7c959ff00c974852a672fd63987b59c84cc8
John DH. Park <john.dh.park@icloud.com>
2023-05-24 09:29:47 -0700

    Successfully used useEffect to retrieve data

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,22 @@
+import React, { useState, useEffect } from "react";
+
 // create your App component here
+function App(props) {
+	const [loaded, setLoaded] = useState(false);
+	const [url, setUrl] = useState("");
+
+	useEffect(() => {
+		fetch("https://dog.ceo/api/breeds/image/random")
+			.then((res) => res.json())
+			.then((data) => {
+				setUrl(data.message);
+				setLoaded(true);
+			});
+	}, []);
+
+	if (!loaded) return <p>Loading...</p>;
+
+	return <img src={url} alt="A Random Dog" />;
+}
+
+export default App;
